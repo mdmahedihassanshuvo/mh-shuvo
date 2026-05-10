d3bd81efe0be88d6825ba0bb20006b87b309df09
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2026-05-10 23:50:36 +0600

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,20 +1,7 @@
 from django.urls import path
 from . import views
 
+# API endpoints only (React handles all page routing)
 urlpatterns = [
-    path(
-        '',
-        views.HomeView.as_view(),
-        name='portfolio_home'
-    ),
-    path(
-        'about/',
-        views.AboutView.as_view(),
-        name='portfolio_about'
-    ),
-    path(
-        'contact/',
-        views.ContactView.as_view(),
-        name='portfolio_contact'
-    ),
+    path('contact/', views.contact, name='api_contact'),
 ]
