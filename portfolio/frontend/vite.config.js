2e4711ec4466a1b2f935328c4d3a6a41eb8e1c9c
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2026-07-11 11:40:21 +0600

    make portfolio project github deployable

--- portfolio/frontend/vite.config.js
+++ portfolio/frontend/vite.config.js
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { defineConfig } from 'vite'
 
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/static/portfolio/' : '/',
-  plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, '../../static/portfolio'),
-    emptyOutDir: true
-  },
-  server: {
-    port: 5000,
-    strictPort: true,
-    proxy: {
-      '/api': 'http://localhost:8000'
+export default defineConfig(({ command, mode }) => {
+  const isGhPages = mode === 'ghpages'
+
+  return {
+    base: isGhPages ? '/mh-shuvo/' : command === 'build' ? '/static/portfolio/' : '/',
+    plugins: [react()],
+    build: {
+      outDir: isGhPages ? path.resolve(__dirname, 'dist') : path.resolve(__dirname, '../../static/portfolio'),
+      emptyOutDir: true
+    },
+    server: {
+      port: 5000,
+      strictPort: true,
+      proxy: {
+        '/api': 'http://localhost:8000'
+      }
     }
   }
-}))
+})
