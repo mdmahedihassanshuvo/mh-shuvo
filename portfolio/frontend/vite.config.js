5d45161d0667ff6a52254b9a8281c42adf1d1ff9
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2026-07-11 11:50:24 +0600

    update the base url

--- portfolio/frontend/vite.config.js
+++ portfolio/frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig(({ command, mode }) => {
   const isGhPages = mode === 'ghpages'
 
   return {
-    base: isGhPages ? '/mh-shuvo/' : command === 'build' ? '/static/portfolio/' : '/',
+    base: isGhPages ? '/' : command === 'build' ? '/static/portfolio/' : '/',
     plugins: [react()],
     build: {
       outDir: isGhPages ? path.resolve(__dirname, 'dist') : path.resolve(__dirname, '../../static/portfolio'),
